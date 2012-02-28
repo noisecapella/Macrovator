4f309bea26db46767e3db00d7a0b7a2897a0504d
George Schneeloch <bostonbusmap@gmail.com>
2012-02-28 17:58:20 -0500

    working on test fixture data

--- db/migrate/20120215232540_create_data.rb
+++ db/migrate/20120215232540_create_data.rb
@@ -2,6 +2,7 @@ class CreateData < ActiveRecord::Migration
   def change
     create_table :data do |t|
       t.text :content
+      t.text :title
       t.integer :user_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120216174117) do
 
   create_table "data", :force => true do |t|
     t.text     "content"
+    t.text     "title"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
