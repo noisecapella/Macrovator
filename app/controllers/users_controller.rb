15841988635ac8839422f63c358efaa847341fb0
George Schneeloch <bostonbusmap@gmail.com>
2012-02-29 17:25:15 -0500

    user/new now redirects to current user page if user is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,11 +24,15 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
-    @user = User.new
-
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
+      if current_user
+        format.html { redirect_to @current_user }
+      else
+        @user = User.new
+      
+        format.html # new.html.erb
+        format.json { render json: @user }
+      end
     end
   end
 
@@ -40,16 +44,19 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    print "YYY",params
-    @user = User.new(params[:user])
-
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+      if @current_user
+        format.html { redirect_to @user }
       else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        @user = User.new(params[:user])
+        
+        if @user.save
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render json: @user, status: :created, location: @user }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
