277b3d37439356a92bb3bc675f71999f50aa335b
George Schneeloch <bostonbusmap@gmail.com>
2012-03-06 16:05:46 -0500

    fixing adding two fields at once

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,5 +23,5 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
     var new_id = new Date().getTime();
     var regexp = new RegExp("new_" + association, "g");
-    $(link).parent().append(content);
+    $(link).parent().append(content.replace(regexp, new_id));
 }
