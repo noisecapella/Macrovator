cd5ed8c6b87ad2bef97ed2919eebe5c5eb755098
George Schneeloch <bostonbusmap@gmail.com>
2012-04-25 18:12:10 -0400

    converting to coffeescript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require data
 
 //  sdf= require_tree .
 
@@ -22,111 +23,3 @@
 //    $(link).parent().append(content.replace(regexp, new_id));
 //}
 
-var KEYBOARD_ARRAY = [];
-var IS_RECORDING = false;
-
-function update_content(data) {
-    var content = $('#content');
-    content.html(data["content"]);
-
-    var info = $('#info');
-    info.html(data["info"]);
-
-    var sidebar = $('#sidebar');
-    sidebar.html(data["sidebar"]);
-}
-
-
-function cursorAnimation()
-{
-   /* $(".cursor").animate(
-	{opacity: 1}).animate(
-	    {opacity: 0});*/
-
-    $(".cursor").animate({borderLeftColor:"#FFFFFF"}).animate({borderLeftColor:"#000000"});
-	
-}
-
-function sendKeystrokes()
-{
-    if (KEYBOARD_ARRAY.length > 0)
-    {
-	// TODO: if this fails, do a check to resend it later
-	$.post("/action_lists/keystrokes", {keys : KEYBOARD_ARRAY});
-	KEYBOARD_ARRAY.splice(0, KEYBOARD_ARRAY.length);
-    }
-
-    setTimeout("sendKeystrokes()", 200);
-}
-
-$(document).ready(function() {
-    setInterval("cursorAnimation()", 600);
-
-    $("#stop_recording").hide();
-
-    $("#action_type_action_type").live('ajax:success', function(evt, data, status, xhr) {
-	    var fields = $('#argument_fields');
-	
-	    fields.html(data);
-	});
-
-
-    $("#execute_link").live("ajax:success", function(evt, data, status, xhr) {
-
-	update_content(data);
-	});
-
-    $("#reset_selected").live("ajax:success", function(evt, data, status, xhr) {
-
-	update_content(data);
-    });
-    $("#clear_link").live("ajax:success", function(evt, data, status, xhr) {
-
-	update_content(data);
-    });
-    $("#record_keystrokes_link").live("ajax:success", function(evt, data, status, xhr) {
-
-	update_content(data);
-    });
-
-    $(document).keydown(function(e)
-			 {
-			     if (IS_RECORDING)
-			     {
-				 KEYBOARD_ARRAY.push({"keydown": e.which});
-				 return false;
-			     }
-			 });
-    $(document).keypress(function(e)
-			 {
-			     if (IS_RECORDING)
-			     {
-				 KEYBOARD_ARRAY.push({"keypress": e.which});
-				 return false;
-			     }
-			 });
-
-    setTimeout("sendKeystrokes()", 200);
-
-});
-
-function bind_record_keystrokes()
-{
-    $("#record_keystrokes_div").bind('click', function() {
-	if (!IS_RECORDING)
-	{
-		
-
-	    $("#record_keystrokes").html('<%= (link_to "Stop recording", url_for(:controller => :action_lists, :action => :status, :id => @datum.action_list.id), :remote => true, :id => "record_keystrokes_link").to_json %>');
-	    IS_RECORDING = true;
-
-	    KEYBOARD_ARRAY = [];
-	}
-	else
-	{
-	    IS_RECORDING = false;
-	    $("#record_keystrokes").html('<%= (link_to "Record keystrokes", url_for(:controller => :action_lists, :action => :status, :id => @datum.action_list.id), :remote => true, :id => "record_keystrokes_link").to_json %>');
-	}
-    });
-
-}
--- app/assets/javascripts/data.js.coffee
+++ app/assets/javascripts/data.js.coffee
@@ -1,3 +1,85 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+root = exports ? this
+
+KEYBOARD_ARRAY = [];
+IS_RECORDING = false;
+
+update_content = (data) ->
+    content = $('#content');
+    content.html(data["content"]);
+
+    info = $('#info');
+    info.html(data["info"]);
+
+    sidebar = $('#sidebar');
+    sidebar.html(data["sidebar"]);
+
+
+
+cursorAnimation = () ->
+    $(".cursor").animate({borderLeftColor:"#FFFFFF"}).animate({borderLeftColor:"#000000"});
+
+sendKeystrokes = () ->
+    if KEYBOARD_ARRAY.length > 0
+        
+        # TODO: if this fails, do a check to resend it later
+        $.post("/action_lists/keystrokes", {keys : KEYBOARD_ARRAY});
+        KEYBOARD_ARRAY.splice(0, KEYBOARD_ARRAY.length);
+
+    setTimeout(sendKeystrokes, 200);
+
+bind_record_keystrokes = () ->
+    $("#record_keystrokes_link").bind('click', () ->
+        alert("true")
+        if !IS_RECORDING
+            $("#record_keystrokes_div").html('<a href="#" onclick="return false;" id="record_keystrokes_link">Stop recording</a>');
+            IS_RECORDING = true;
+
+            KEYBOARD_ARRAY = [];
+        else
+            IS_RECORDING = false;
+            $("#record_keystrokes_div").html('<a href="#" onclick="return false;" id="record_keystrokes_link">Record keystrokes</a>');
+        bind_record_keystrokes()
+    );
+
+
+$ ->
+    setInterval(cursorAnimation, 600);
+
+    $("#stop_recording").hide();
+
+    $("#action_type_action_type").live('ajax:success', (evt, data, status, xhr) ->
+            fields = $('#argument_fields');
+        
+            fields.html(data);
+        );
+
+    do_update_content = (evt, data, status, xhr) ->
+        update_content(data);
+    
+
+    $("#execute_link").live("ajax:success", do_update_content)
+
+    $("#reset_selected").live("ajax:success", do_update_content)
+    $("#clear_link").live("ajax:success", do_update_content)
+    $("#record_keystrokes_link").live("ajax:success", do_update_content)
+
+    $(document).keydown((e) ->
+                             if IS_RECORDING
+                                 KEYBOARD_ARRAY.push({"keydown": e.which});
+                                 false
+                             );
+    $(document).keypress((e) ->
+                             if IS_RECORDING
+                                 KEYBOARD_ARRAY.push({"keypress": e.which});
+                                 false
+                         );
+
+    setTimeout(sendKeystrokes, 200);
+    bind_record_keystrokes()
+
+root.bind_record_keystrokes = bind_record_keystrokes
+
