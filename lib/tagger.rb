b90629d8dd5b2b20dc55b8957ce0e36ad231a8eb
George Schneeloch <bostonbusmap@gmail.com>
2012-04-26 14:57:17 -0400

    tagger improvements

--- lib/tagger.rb
+++ lib/tagger.rb
@@ -12,6 +12,15 @@ class Tagger
     @insertions << s
   end
 
+  def add_global_replace(from, to, is_html_safe)
+    @content.to_enum(:scan, from).map do |m,|
+      index = $`.size
+      s = InsertionPoint.new(to, index, is_html_safe)
+      
+      @insertions << s
+    end
+  end
+
   def display_content
     sorted_insertions = @insertions.sort { |a, b| a.pos <=> b.pos }.reverse
 
